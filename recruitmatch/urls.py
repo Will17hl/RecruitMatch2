af457d233865aeb8c5039569939f184576b71686
Nicolas <zapatanicolas095@gmail.com>
2025-04-03 17:06:21 -0500

    Base de datos poblada

--- recruitmatch/urls.py
+++ recruitmatch/urls.py
@@ -18,8 +18,13 @@ from django.contrib import admin
 from django.urls import path
 from vacantes import views as vacanteViews
 
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', vacanteViews.home, name='home'),
     path('signup/', vacanteViews.signup, name='signup'),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

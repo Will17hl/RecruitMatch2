7a7c518a4e051286c882eddc2bac96c302f14cd7
Nicolas <zapatanicolas095@gmail.com>
2025-04-03 17:06:21 -0500

    Base de datos poblada

--- vacantes/models.py
+++ vacantes/models.py
@@ -3,7 +3,9 @@ from django.db import models
 class Vacante(models.Model):
     title = models.CharField(max_length=100)
     description = models.CharField(max_length=250, null=True, blank=True)
-    image = models.ImageField(upload_to ='movie/images/')
+    image = models.ImageField(upload_to='vacante/images/', null=True, blank=True, default='vacante/images/default.jpeg')
+    company = models.CharField(max_length=100, null=True, blank=True)
+    location = models.CharField(max_length=100, null=True, blank=True)
     area = models.CharField(blank=True, max_length=250)
     date = models.DateField(blank=True, null=True)
     objects = models.Manager()
--- vacantes/views.py
+++ vacantes/views.py
@@ -1,13 +1,37 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from .models import Vacante
 
 def home(request):
-    searchVacante = request.GET.get('searchMovie')
-    if searchVacante:
-        vacantes = Vacante.objects.filter(title__icontains=searchVacante)
-    else:    
-        vacantes = Vacante.objects.all()
-    return render(request, 'home.html', {'searchVacante':searchVacante, 'vacantes': vacantes})  
+    vacantes = Vacante.objects.all()
 
+    # Get search parameters
+    search_query = request.GET.get('searchvacante', '')
+    company_filter = request.GET.get('company', '')
+    location_filter = request.GET.get('location', '')
+    area_filter = request.GET.get('area', '')
+
+    # Apply filters
+    if search_query:
+        vacantes = vacantes.filter(title__icontains=search_query)
+    if company_filter:
+        vacantes = vacantes.filter(company__icontains=company_filter)
+    if location_filter:
+        vacantes = vacantes.filter(location__icontains=location_filter)
+    if area_filter:
+        vacantes = vacantes.filter(area__icontains=area_filter)
+
+    # Paginate results
+    paginator = Paginator(vacantes, 12)  # Show 10 jobs per page
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'home.html', {
+        'page_obj': page_obj,
+        'searchvacante': search_query,
+        'company': company_filter,
+        'location': location_filter,
+        'area': area_filter,
+    })
 def signup(request):
     return render(request, 'signup.html')
